fe8d0084dd55154cd727d54c2c46e91c254d8945
Gregorio <gregorio@fefland.com>
2018-01-20 20:27:10 -0700

    gang

---
+++ garageSale.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Gregorio on 1/20/2018.
+//
+
+
+struct item
+{
+
+};
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+Gregorio Figueroa
+CSCI 2270 Data Structures
+Assignment 1
+ */
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+}
